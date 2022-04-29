fe6019d18057959abc729b1c398c16ad03db7227
Ericsson Colborn <ericssoncolborn@Ericssons-Air.nyc.rr.com>
2022-04-29 11:54:41 -0400

    changed proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/api/',
     createProxyMiddleware({
       target: 'http://mytunes-api.herokuapp.com',
-      changeOrigin: false,
+      changeOrigin: true,
     })
   );
 };

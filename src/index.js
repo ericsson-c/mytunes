9629409ada6056e5c5b8080822da6c57ba88751a
Ericsson Colborn <ericssoncolborn@Ericssons-Air.cable.rcn.com>
2022-08-08 18:48:27 +0000

    changed name of production env to just .env

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
 import { CookiesProvider } from 'react-cookie';
-import 'dotenv-webpack';
 
 //import reportWebVitals from './reportWebVitals';
 
